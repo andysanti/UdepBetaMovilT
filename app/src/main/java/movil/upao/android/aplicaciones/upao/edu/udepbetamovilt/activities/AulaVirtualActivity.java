339568fa64de218d1899708d61cb008fa1bad019
Lion <josue.joshua.ly@gmail.com>
2018-11-04 11:06:37 -0500

    Avance de Asistencias

--- app/src/main/java/movil/upao/android/aplicaciones/upao/edu/udepbetamovilt/activities/AulaVirtualActivity.java
+++ app/src/main/java/movil/upao/android/aplicaciones/upao/edu/udepbetamovilt/activities/AulaVirtualActivity.java
@@ -1,15 +1,31 @@
 package movil.upao.android.aplicaciones.upao.edu.udepbetamovilt.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 import movil.upao.android.aplicaciones.upao.edu.udepbetamovilt.R;
 
-public class AulaVirtualActivity extends AppCompatActivity {
+public class AulaVirtualActivity extends AppCompatActivity  implements View.OnClickListener {
 
+    private CardView crdAsistencias;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aula_virtual);
+
+        crdAsistencias= findViewById(R.id.crdAsistencia);
+        crdAsistencias.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, AsistenciaActivity.class);
+        startActivity(intent);
+    }
+
+
 }
